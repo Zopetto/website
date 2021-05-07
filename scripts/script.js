71cfed6ad046b36f39cc05e415d2c69226540ae8
7oe <59398669+7oe@users.noreply.github.com>
2021-05-07 23:25:39 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,109 @@
-function copyTag() {
-    console.log("boobies")
-    const tag = document.getElementsByClassName("discord")[0]
+var Typer = {
+  text: '',
+  accessCountimer: null,
+  index: 0,
+  speed: 2,
+  file: '',
+  accessCount: 0,
+  deniedCount: 0,
+  init: function () {
+    accessCountimer = setInterval(function () {
+      Typer.updLstChr();
+    }, 500);
+    $.get(Typer.file, function (data) {
+      Typer.text = data;
+      Typer.text = Typer.text.slice(0, Typer.text.length - 1);
+    });
+  },
 
-    if (tag.innerText != "zope70777") return
+  content: function () {
+    return $('#console').html();
+  },
 
-    const el = document.createElement("textarea")
-    el.value = "zope#7777"
-    document.body.appendChild(el)
-    el.select()
-    document.execCommand("copy")
-    document.body.removeChild(el)
+  write: function (str) {
+    $('#console').append(str);
+    return false;
+  },
 
-    tag.innerText = "copied"
+  addText: function (key) {
+    if (key.keyCode == 18) {
+      Typer.accessCount++;
 
-    setTimeout(() => {
-        tag.innerText = "zope#7777"
-    }, 1000)
+      if (Typer.accessCount >= 3) {
+        Typer.makeAccess();
+      }
+    } else if (key.keyCode == 20) {
+      Typer.deniedCount++;
+
+      if (Typer.deniedCount >= 3) {
+        Typer.makeDenied();
+      }
+    } else if (key.keyCode == 27) {
+      Typer.hidepop();
+    } else if (Typer.text) {
+      var cont = Typer.content();
+      if (cont.substring(cont.length - 1, cont.length) == '|')
+        $('#console').html(
+          $('#console')
+            .html()
+            .substring(0, cont.length - 1),
+        );
+      if (key.keyCode != 8) {
+        Typer.index += Typer.speed;
+      } else {
+        if (Typer.index > 0) Typer.index -= Typer.speed;
+      }
+      var text = Typer.text.substring(0, Typer.index);
+      var rtn = new RegExp('\n', 'g');
+
+      $('#console').html(text.replace(rtn, '<br/>'));
+      window.scrollBy(0, 50);
+    }
+
+    if (key.preventDefault && key.keyCode != 122) {
+      key.preventDefault();
+    }
+
+    if (key.keyCode != 122) {
+      // otherway prevent keys default behavior
+      key.returnValue = false;
+    }
+  },
+
+  updLstChr: function () {
+    var cont = this.content();
+
+    if (cont.substring(cont.length - 1, cont.length) == '|')
+      $('#console').html(
+        $('#console')
+          .html()
+          .substring(0, cont.length - 1),
+      );
+    else this.write('|'); // else write it
+  },
+};
+
+function replaceUrls(text) {
+  var http = text.indexOf('http://');
+  var space = text.indexOf('.me ', http);
+
+  if (space != -1) {
+    var url = text.slice(http, space - 1);
+    return text.replace(url, '<a href="' + url + '">' + url + '</a>');
+  } else {
+    return text;
+  }
+}
+
+Typer.speed = 3;
+Typer.file = 'zope.txt';
+Typer.init();
+
+var timer = setInterval('t();', 30);
+function t() {
+  Typer.addText({ keyCode: 123748 });
+
+  if (Typer.index > Typer.text.length) {
+    clearInterval(timer);
+  }
 }
