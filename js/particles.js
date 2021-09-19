6982298546ced19d82e440a9b89291225b629e0f
7oe <59398669+7oe@users.noreply.github.com>
2021-09-19 04:10:27 +0100

    Update particles.js

--- js/particles.js
+++ js/particles.js
@@ -19,114 +19,116 @@ var pJS = function (tag_id, params) {
         w: canvas_el.offsetWidth,
         h: canvas_el.offsetHeight,
       },
-      particles: {
-        number: {
-          value: 600,
-          density: {
-            enable: true,
-            value_area: 800,
-          },
-        },
-        color: {
-          value: "#f000ff",
-        },
-        shape: {
-          type: "le",
-          stroke: {
-            width: 0,
-            color: "#ff0000",
-          },
-          polygon: {
-            nb_sides: 5,
-          },
-          image: {
-            src: "",
-            width: 100,
-            height: 100,
-          },
-        },
-        opacity: {
-          value: 1,
-          random: false,
-          anim: {
-            enable: false,
-            speed: 2,
-            opacity_min: 0,
-            sync: false,
-          },
-        },
-        size: {
-          value: 20,
-          random: false,
-          anim: {
-            enable: false,
-            speed: 20,
-            size_min: 0,
-            sync: false,
-          },
-        },
-        line_linked: {
-          enable: true,
-          distance: 100,
-          color: "#fff",
-          opacity: 1,
-          width: 1,
-        },
-        move: {
-          enable: true,
-          speed: 2,
-          direction: "none",
-          random: false,
-          straight: false,
-          out_mode: "out",
-          bounce: false,
-          attract: {
-            enable: false,
-            rotateX: 3000,
-            rotateY: 3000,
-          },
-        },
-        array: [],
+      {
+  "particles": {
+    "number": {
+      "value": 118,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#0000aa"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#ff0000"
       },
-      interactivity: {
-        detect_on: "canvas",
-        events: {
-          onhover: {
-            enable: true,
-            mode: "grab",
-          },
-          onclick: {
-            enable: true,
-            mode: "push",
-          },
-          resize: true,
-        },
-        modes: {
-          grab: {
-            distance: 100,
-            line_linked: {
-              opacity: 1,
-            },
-          },
-          bubble: {
-            distance: 200,
-            size: 80,
-            duration: 0.4,
-          },
-          repulse: {
-            distance: 200,
-            duration: 0.4,
-          },
-          push: {
-            particles_nb: 4,
-          },
-          remove: {
-            particles_nb: 2,
-          },
-        },
-        mouse: {},
+      "polygon": {
+        "nb_sides": 6
       },
-      retina_detect: false,
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5287289198936227,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 157.82952832645452,
+      "color": "#15c2f7",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+}
       fn: {
         interact: {},
         modes: {},
