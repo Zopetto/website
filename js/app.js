ecc39b7579336838a233238bd13dc11176187fb7
7oe <59398669+7oe@users.noreply.github.com>
2021-11-10 03:08:27 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,57 +15,57 @@ particlesJS(
     "particles-js",
   
     {
-    "particles": {
-        "number": {
-            "value": 30
-        },
-        "color": {
-            "value": "#4034eb"
-        },
-        "shape": {
-            "type": "circle",
-            "stroke": {
-                "width": 0,
-                "color": "#000000"
-            },
-            "polygon": {
-                "nb_sides": 1
-            }
-        },
-        "opacity": {
-            "value": 0.5,
-            "random": false,
-            "anim": {
-                "enable": false,
-                "speed": 1,
-                "opacity_min": 0.1,
-                "sync": false
-            }
-        },
-        "size": {
-            "value": 5,
-            "random": true
-        },
-        "line_linked": {
-            "enable": true,
-            "distance": 150,
-            "color": "#9e34eb",
-            "opacity": 0.4,
-            "width": 2
-        },
-        "move": {
-            "enable": true,
-            "speed": 12,
-            "direction": "none",
-            "random": false,
-            "straight": false,
-            "out_mode": "out",
-            "bounce": false,
-            "attract": {
-                "enable": false,
-                "rotateX": 600,
-                "rotateY": 1200
-            }
-        }
-    }
-}
+      "particles": {
+          "number": {
+              "value": 60
+          },
+          "color": {
+              "value": "#0077b6"
+          },
+          "shape": {
+              "type": "circle",
+              "stroke": {
+                  "width": 0,
+                  "color": "#000000"
+              },
+              "polygon": {
+                  "nb_sides": 1
+              }
+          },
+          "opacity": {
+              "value": 0.5,
+              "random": false,
+              "anim": {
+                  "enable": false,
+                  "speed": 1,
+                  "opacity_min": 0.1,
+                  "sync": false
+              }
+          },
+          "size": {
+              "value": 5,
+              "random": true
+          },
+          "line_linked": {
+              "enable": true,
+              "distance": 150,
+              "color": "#00b4d8",
+              "opacity": 0.4,
+              "width": 2
+          },
+          "move": {
+              "enable": true,
+              "speed": 12,
+              "direction": "none",
+              "random": false,
+              "straight": false,
+              "out_mode": "out",
+              "bounce": false,
+              "attract": {
+                  "enable": false,
+                  "rotateX": 600,
+                  "rotateY": 1200
+              }
+          }
+      }
+  });
