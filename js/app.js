a733ed07f329bb04011691327871aa92e6c8d7ab
7oe <59398669+7oe@users.noreply.github.com>
2022-01-10 21:03:18 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -55,7 +55,7 @@ particlesJS(
           },
           "move": {
               "enable": true,
-              "speed": 12,
+              "speed": 8,
               "direction": "none",
               "random": false,
               "straight": false,
